00c3405b205dd9b6025dac83cdfde2f2a44854f2
juan-pestana <juan@mai-solutions.net>
2025-07-13 03:01:27 +0200

    see non suspended useSearchParams fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false
+  }
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
